8c3502c24c4264af9d577dc7e7db5aec51ef327d
Elian <d2099080@gmail.com>
2024-02-27 19:52:30 +0800

    Implement q_remove_head, q_remove_tail
    [What?]
    Implement a function to remove and return a node from the head
    or tail of the queue.
    
    [Why?]
    To allow nodes to be removed from the head or tail of the queue.
    
    [How?]
    First, check if the queue exists or if it is empty.
    Then, use list_first_entry or list_last_entry to get the nodes at
    the head or tail of the queue, and copy the string from
    the node to sp.
    Finally, use list_del to remove the node from the queue
    and return the node.

--- queue.c
+++ queue.c
@@ -62,13 +62,25 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+    element_t *tmp = list_first_entry(head, element_t, list);
+    strncpy(sp, tmp->value, bufsize);
+    sp[bufsize - 1] = '\0';
+    list_del(&tmp->list);
+    return tmp;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+    element_t *tmp = list_last_entry(head, element_t, list);
+    strncpy(sp, tmp->value, bufsize);
+    sp[bufsize - 1] = '\0';
+    list_del(&tmp->list);
+    return tmp;
 }
 
 /* Return number of elements in queue */
