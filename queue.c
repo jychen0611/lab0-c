3453f215bc4379f713d16a45181a1f90884caacb
Elian <d2099080@gmail.com>
2024-02-28 14:43:21 +0800

    Implement q_reverse
    [What?]
    Implement a function to reverse the order of all nodes in the queue.
    
    [Why?]
    To allow the queue to change direction.
    
    [How?]
    Iteratively reverse the direction of all links in the queue.
    Also, note that once the pointer l reaches the last node, the program
    exits the loop, so an additional action inside the loop should be
    added.

--- queue.c
+++ queue.c
@@ -194,7 +194,18 @@ void q_swap(struct list_head *head)
 }
 
 /* Reverse elements in queue */
-void q_reverse(struct list_head *head) {}
+void q_reverse(struct list_head *head)
+{
+    if (!head || list_empty(head))
+        return;
+    struct list_head *r, *l;
+    list_for_each_safe (l, r, head) {
+        l->next = l->prev;
+        l->prev = r;
+    }
+    l->next = l->prev;
+    l->prev = r;
+}
 
 /* Reverse the nodes of the list k at a time */
 void q_reverseK(struct list_head *head, int k)
